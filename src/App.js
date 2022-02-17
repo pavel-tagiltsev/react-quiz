31515251afa6ab71e9d47dafab8d76d60af6a06f
Павел <pavel.tagiltsef@gmail.com>
2022-02-17 09:38:29 +0300

    Setting Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import Layout from './hoc/Layout/Layout'
-import Quize from './container/Quiz/Quiz';
+import {Switch, Route} from 'react-router-dom'
+import Quiz from './container/Quiz/Quiz';
+import QuizList from './container/QuizList/QuizList';
+import QuizCreator from './container/QuizCreator/QuizCreator';
+import Auth from './container/Auth/Auth';
 
 function App() {
   return (
     <div className="App">
       <Layout>
-        <Quize />
+        <Switch>
+          <Route path='/auth' component={Auth}/>
+          <Route path='/quiz-creator' component={QuizCreator}/>
+          <Route path='/quiz/:id' component={Quiz}/>
+          <Route path='/' component={QuizList}/>
+        </Switch>
       </Layout>
     </div>
   );
