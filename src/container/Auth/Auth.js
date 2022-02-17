a978ddf9ecca6c3cf692369d6cb2d5d7a5e6e24b
Павел <pavel.tagiltsef@gmail.com>
2022-02-17 16:10:40 +0300

    Registration Page

--- src/container/Auth/Auth.js
+++ src/container/Auth/Auth.js
@@ -1,10 +1,46 @@
-import {Component} from 'react'
+import React, {Component} from 'react'
+import classes from './Auth.module.css'
+import Button from '../../components/UI/Button/Button'
 
 export default class Auth extends Component {
+
+  loginHandler = () => {
+
+  }
+
+  registerHandler = () => {
+
+  }
+
+  submitHandler = (evt) => {
+    evt.preventDefault()
+  }
+
   render() {
     return (
-      <div>
-        <h1>Auth</h1>
+      <div className={classes.Auth}>
+        <div>
+          <h1>Авторизация</h1>
+
+          <form onSubmit={this.submitHandler} className={classes.AuthForm}>
+            <input type="text"/>
+            <input type="text"/>
+
+            <Button
+              type="success"
+              onClick={this.loginHandler}
+            >
+              Войти
+            </Button>
+
+            <Button
+              type="primary"
+              onClick={this.registerHandler}
+            >
+              Зарегистрироваться
+            </Button>
+          </form>
+        </div>
       </div>
     )
   }
