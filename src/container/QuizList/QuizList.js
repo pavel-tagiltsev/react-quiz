3883a8c41146fb309009d361efc5f84ada24f14e
Павел <pavel.tagiltsef@gmail.com>
2022-02-18 19:39:46 +0300

    Loading the list of tests

--- src/container/QuizList/QuizList.js
+++ src/container/QuizList/QuizList.js
@@ -1,21 +1,47 @@
 import React, {Component} from 'react'
 import classes from './QuizList.module.css'
+import axios from 'axios'
 import {NavLink} from 'react-router-dom'
 
 export default class QuizList extends Component {
 
+  state = {
+    quizes: []
+  }
+
   renderQuizes() {
-    return [1, 2, 3].map((quiz, index) => {
+    return this.state.quizes.map((quiz) => {
       return (
         <li
-          key={index}
+          key={quiz.id}
         >
-          <NavLink to={'/quiz/' + quiz}>
-            Тест {quiz}
+          <NavLink to={'/quiz/' + quiz.id}>
+            {quiz.name}
           </NavLink>
         </li>
       )
     })
+  }
+
+  async componentDidMount() {
+    try {
+      const response = await axios.get('https://react-quiz-18612-default-rtdb.europe-west1.firebasedatabase.app/quizes.json')
+
+      const quizes = []
+      
+      Object.keys(response.data).forEach((key, index) => {
+        quizes.push({
+          id: key,
+          name: `Тест №${index + 1}`
+        })
+      })
+
+      this.setState({
+        quizes
+      })
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   render() {
